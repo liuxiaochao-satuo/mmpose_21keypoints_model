b3e1ffaff51c5d9a75673adeea2a926dcbaae264
liuxiaochao-satuo <2799851651@qq.com>
2026-02-03 10:39:14 +0800

    删除全部文件

--- mmpose/datasets/samplers/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-# Note: MultiSourceSampler is defined in ../samplers.py (parent directory)
-# This __init__.py only exports WeightedGroupSampler
-from .weighted_group_sampler import WeightedGroupSampler
-
-__all__ = ['WeightedGroupSampler']
-
